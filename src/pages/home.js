e4e920faee0c0544b23e0292f98bcf08347f0a8e
kimminji-1130 <ddaa637878@gmail.com>
2023-05-08 19:07:25 +0900

    make a basic homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import './Home.css';
 
 const Home = () => {
   return (
+    <div>
     <div className='header'>
       <div className='about'>ABOUT</div>
       <div className='service'>SERVICE</div>
@@ -12,7 +13,27 @@ const Home = () => {
       <div className='slog'>SLOG</div>
       <div className='contact'>CONTACT</div>
       <div className='welcome'>WELLCOME</div>
-      <div className='welcome'>WELLCOME</div>
+      <div className='contant'>f you still encounter the "Missing script</div>
+      <div className='text'>f you still encounter the "Missing script: 'start'" error after following these steps, please check the contents</div>
+      <div className='text1'>f you still encounter the "Missing script: 'start'" error after following these steps</div>
+      <div className='contants_1'>SERVICES</div>
+      <div className='text1_1'> the Missing script</div>
+      <div className='text1_2'>f you still encounter the "Missing script: 'start'" error after following</div>
+      <div className='text1_3'>f you still encounter the "Missing script: 'start'" error after following</div>
+      <div className='text1_4'>f you still encounter the "Missing script: 'start'" error after</div>
+      <div className='text1_5'>f you still encounter the "Missing script: 'start'" error after</div>
+      <div className='picture1'></div>
+      <div className='contants_2'>SERVICES</div>
+      <div className='text2_1'>the Missing script</div>
+      <div className='text2_2'>f you still encounter the "Missing script: 'start'" error after following</div>
+      <div className='text2_3'>f you still encounter the "Missing script: 'start'" error after following</div>
+      <div className='text2_4'>f you still encounter the "Missing script: 'start'" error after following</div>
+      <div className='text2_5'>f you still encounter the "Missing script: 'start'" error after</div>
+      <div className='picture2'></div>
+      <div className='footer'>
+
+      </div>
+    </div>
     </div>
   );
 }
