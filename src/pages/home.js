90b25d566fdee76bc1355bfaa6c5978bf529d3a9
kimminji-1130 <ddaa637878@gmail.com>
2023-05-09 21:31:32 +0900

    link 연결 완료

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,56 +1,55 @@
 import React from 'react';
+import { BrowserRouter, Link } from "react-router-dom";
 import './Home.css';
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+export function Home() {
+    return (
+        <BrowserRouter>
+            <div className='header'>
+                <div className='about'>
+                    <Link to='/'>ABOUT</Link>
+                </div>
+                <div className='service'>
+                    <Link to='/'>SERVICE</Link>
+                </div>
+                <div className='work'>
+                    <Link to='/'>WORK</Link>
+                </div>
+                <div className='name'>
+                    <Link to='/'>MINJI</Link>
+                </div>
+                <div className='shop'>
+                    <Link to='/'>SHOP</Link>
+                </div>
+                <div className='slog'>
+                    <Link to='/'>SLOG</Link>
+                </div>
+                <div className='contact'>
+                    <Link to='/'>SERVICE</Link>
+                </div>
+                <div className='welcome'>WELLCOME</div>
+                <div className='contant'>f you still encounter the "Missing script</div>
+                <div className='text'>f you still encounter the "Missing script: 'start'" error after following these steps, please check the contents</div>
+                <div className='text1'>f you still encounter the "Missing script: 'start'" error after following these steps</div>
+                <div className='contants_1'>SERVICES</div>
+                <div className='text1_1'> the Missing script</div>
+                <div className='text1_2'>f you still encounter the "Missing script: 'start'" error after following</div>
+                <div className='text1_3'>f you still encounter the "Missing script: 'start'" error after following</div>
+                <div className='text1_4'>f you still encounter the "Missing script: 'start'" error after</div>
+                <div className='text1_5'>f you still encounter the "Missing script: 'start'" error after</div>
+                <div className='picture1'></div>
+                <div className='contants_2'>SERVICES</div>
+                <div className='text2_1'>the Missing script</div>
+                <div className='text2_2'>f you still encounter the "Missing script: 'start'" error after following</div>
+                <div className='text2_3'>f you still encounter the "Missing script: 'start'" error after following</div>
+                <div className='text2_4'>f you still encounter the "Missing script: 'start'" error after following</div>
+                <div className='text2_5'>f you still encounter the "Missing script: 'start'" error after</div>
+                <div className='picture2'></div>
+                <div className='footer'>
 
-const Home = () => {
-  return (
-    <BrowserRouter>
-    <div className='header'>
-      <div className='about'>
-        <Link to='/'>ABOUT</Link>
-      </div>
-      <div className='service'>
-      <Link to='/'>SERVICE</Link>
-      </div>
-      <div className='work'>
-      <Link to='/'>WORK</Link>
-      </div>
-      <div className='name'>
-      <Link to='/'>MINJI</Link>
-      </div>
-      <div className='shop'>
-      <Link to='/'>SHOP</Link>
-      </div>
-      <div className='slog'>
-      <Link to='/'>SLOG</Link>
-      </div>
-      <div className='contact'>
-      <Link to='/'>SERVICE</Link>
-      </div>
-      <div className='welcome'>WELLCOME</div>
-      <div className='contant'>f you still encounter the "Missing script</div>
-      <div className='text'>f you still encounter the "Missing script: 'start'" error after following these steps, please check the contents</div>
-      <div className='text1'>f you still encounter the "Missing script: 'start'" error after following these steps</div>
-      <div className='contants_1'>SERVICES</div>
-      <div className='text1_1'> the Missing script</div>
-      <div className='text1_2'>f you still encounter the "Missing script: 'start'" error after following</div>
-      <div className='text1_3'>f you still encounter the "Missing script: 'start'" error after following</div>
-      <div className='text1_4'>f you still encounter the "Missing script: 'start'" error after</div>
-      <div className='text1_5'>f you still encounter the "Missing script: 'start'" error after</div>
-      <div className='picture1'></div>
-      <div className='contants_2'>SERVICES</div>
-      <div className='text2_1'>the Missing script</div>
-      <div className='text2_2'>f you still encounter the "Missing script: 'start'" error after following</div>
-      <div className='text2_3'>f you still encounter the "Missing script: 'start'" error after following</div>
-      <div className='text2_4'>f you still encounter the "Missing script: 'start'" error after following</div>
-      <div className='text2_5'>f you still encounter the "Missing script: 'start'" error after</div>
-      <div className='picture2'></div>
-      <div className='footer'>
-
-      </div>
-    </div>
-    </BrowserRouter>
-  );
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default Home;
