d802d0505949389c585d8198d4eb76ae429ef3b6
kimminji-1130 <ddaa637878@gmail.com>
2023-05-10 13:08:31 +0900

    commit message

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,10 +4,28 @@ import './About.css';
 function About() {
     return (
         <div className="about-page">
-            <h1>About</h1>
-            <p>This is the About page.</p>
-            <Link to="/">Go to Home</Link>
+            <div className='blank'></div>
+            <div className='link'>
+            <div className='minji'><a href="/">MINJI</a></div>
+            </div>
+            <div className='firstmenu'></div>
+            <div className='secondmenu_picture'></div>
+            <div className='secondmenu_line'></div>
+            <div className='thirdmenu'>OUR OVERVIEW</div>
+            <div className='third_picture1'></div>
+            <div className='third_picture2'></div>
+            <div className='third_picture3'></div>
+            <div className='third_picture4'></div>
+            <div className='third_picture5'></div>
+            <div className='fifthmenu_line'>
+                <div className='fifthmenu_picture1'></div>
+                <div className='fifthmenu_picture2'></div>
+                <div className='fifthmenu_picture3'></div>
+                <div className='fifthmenu_picture4'></div>
+            </div>
+            <div className='footer'></div>
         </div>
+
     );
 }
 
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import './Service.css';
 
 function Service() {
   return (
     <div>
-      <h1>Service</h1>
-      <p>This is the Service page.</p>
-      <Link to="/">Go to Home</Link>
+      <div className='minji'><a href="/">MINJI</a></div>
+      <div className='smalltext'>minjiminjiminjiminji</div>
+      <div className='bigtext'>1:1  맞춤식 종합교육 시스템</div>
+      <div className='bigpicture'></div>
+      <div className='sidemenu'></div>
+      <div className='secondpic1'></div>
+      <div className='secondpic2'></div>
+      <div className='secondpic3'></div>
+      <div className='secondtext'>설명설명설명설명설명설명설명설명설명설명설명설명 설명설명설명
+        설명설명설명설명설명설명설명설명설명설명설명설명 설명설
+        설명설명설명설명설명설명설명설명</div>
+      <div className='third'>
+
+      </div>
+      <div className='fourthtext'>
+        어떤 사람들에게 항상 열려 있습니다.어쩌구 저쩌구 이러쿵 저러쿵쿵
+        </div>
+      <div className='fourthmap'></div>
+      <div className='footer'></div>
     </div>
   );
 }
