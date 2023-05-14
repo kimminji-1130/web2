a417835fe5f3ac7be801288e93320d8d407ed3a6
kimminji-1130 <ddaa637878@gmail.com>
2023-05-14 23:29:30 +0900

    about fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,17 +12,32 @@ function About() {
             <Link to="/Work" className="work">WORK</Link>
             <Link to="/" className="minji">MINJI</Link>
             <Link to="/Shop" className="shop">SHOP</Link>
+            <Link to="/Slog" className="slog">SLOG</Link>
             <Link to="/Contact" className="contact">CONTACT</Link>
-            <div className='firstmenu'></div>
+            <div className='firstmenu'>
+                <div className='firsttext1'>어쩌구 저쩌구 혁신 어쩌구</div>
+                <div className='firsttext2'>어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 어쩌구저쩌 구구어ㅓㅉ구저쩌구 </div>
+            </div>
             <div className='secondmenu_picture'></div>
             <div className='secondmenu_line'></div>
-            <div className='thirdmenu'>OUR OVERVIEW</div>
+            <div className='secondmenu1'>OUR OVERVIEW</div>
+            <div className='secondmenu2'>OVERVIEW</div>
+            <div className='second_line1'></div>
+            <div className='second_line2'></div>
+            <div className='second_line3'></div>
+            <div className='second_line4'></div>
+            <div className='second_line5'></div>
+
+            <div className='thirdmenu1'>OUR OVERVIEW</div>
+            <div className='thirdmenu2'>OVERVIEW</div>
             <div className='third_picture1'></div>
             <div className='third_picture2'></div>
             <div className='third_picture3'></div>
             <div className='third_picture4'></div>
             <div className='third_picture5'></div>
             <div className='fifthmenu_line'>
+                <div className='fifthmenu1'>OUR OVERVIEW</div>
+                <div className='fifthmenu2'>OVERVIEW</div>
                 <div className='fifthmenu_picture1'></div>
                 <div className='fifthmenu_picture2'></div>
                 <div className='fifthmenu_picture3'></div>
